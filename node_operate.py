22825bd974a35084b3f4681f43ad8440e4941d20
Leon Wong <shineyao0221@163.com>
2016-12-23 16:39:35 +0800

    1st coordinate

--- node_operate.py
+++ node_operate.py
@@ -4,60 +4,41 @@
 class Node:
     'Good luck! Have fun!'
 
-    node_data = []
-    #用于存储读取的NODE编号和坐标
-    a_line = []
-    #用于存放文档所有数据
-    ls = []
-    le = []
-    #ls,le用于记录a_line中Node的起始、结束位置
-
-    def __init__(self, path):
-    #类初始化，根据path读取指定的*.inp文件并提取参数        
-        fo = open(path, 'r')
-        #打开文件
-        Node.a_line = fo.readlines()
-        #遍历文档的数据
-        for i in range(len(Node.a_line)):
-            if Node.a_line[i].startswith('*NODE'):
-                Node.ls = i+1
-                #NODE数据开始行
+    def __init__(self, input_path):   #类初始化，根据path读取指定的*.inp文件并提取参数
+        self.node_data = []
+        fo = open(input_path, 'r')     #打开文件
+        self.inp_data = fo.readlines()  #遍历文档的数据
+        for i in range(len(self.inp_data)):
+            if self.inp_data[i].startswith('*NODE'):
+                self.ls = i+1       #NODE数据开始行
                 break
-        for i in range(Node.ls,len(Node.a_line)):
-            if Node.a_line[i].startswith('*'):
-                Node.le = i
-                #NODE数据结束行+1
+        for i in range(self.ls, len(self.inp_data)):
+            if self.inp_data[i].startswith('*'):
+                self.le = i     #NODE数据结束行+1
                 break
-            Node.node_data.append(map(float,Node.a_line[i].split(',')))
-            #将NODE数据读取转换后加入数组
+            temp_data = self.inp_data[i].split(',')
+            self.node_data.append([int(temp_data[0]), float(temp_data[1]), float(temp_data[2]), float(temp_data[3])])
+            #self.node_data.append(int(temp_data[0]).extend)
+            #self.node_data.append(map(float, self.inp_data[i].split(',')))       #将NODE数据读取转换后加入数组
         fo.close()
         
-    def shift(self, node_id, dx=0, dy=0, dz=0):
-    #类成员，根据提供的NODE编号和位移移动指定NODE
-        if node_id == 'all':
-            for i in range(0,Node.le-Node.ls):
-                Node.node_data[i][1] += dx
-                Node.node_data[i][2] += dy
-                Node.node_data[i][3] += dz
-                Node.a_line[i+Node.ls] = str(Node.node_data[i]).replace('[','').replace(']','\n')
+    def shift(self, node_id, dx=0, dy=0, dz=0):  #类成员，根据提供的NODE编号和位移移动指定NODE
+        if node_id == 'all':    #根据不同情况设定范围
+            id_range = [0, self.le-self.ls]
         elif type(node_id) == list or type(node_id) == tuple:
-            for i in range(node_id[0]-1,node_id[1]):
-                Node.node_data[i][1] += dx
-                Node.node_data[i][2] += dy
-                Node.node_data[i][3] += dz
-                Node.a_line[i+Node.ls] = str(Node.node_data[i]).replace('[','').replace(']','\n')
+            id_range = [node_id[0]-1, node_id[1]]
         elif type(node_id) == int:
-                Node.node_data[node_id-1][1] += dx
-                Node.node_data[node_id-1][2] += dy
-                Node.node_data[node_id-1][3] += dz
-                Node.a_line[node_id-1+Node.ls] = str(Node.node_data[node_id-1]).replace('[','').replace(']','\n')
+            id_range = [node_id - 1, node_id]
+        for i in range(id_range[0], id_range[1]):
+            self.node_data[i][1] += dx
+            self.node_data[i][2] += dy
+            self.node_data[i][3] += dz
+            self.inp_data[i+self.ls] = str(self.node_data[i]).replace('[', '').replace(']', '\n')
         
-    def set_nset(self, nset):
-    #类成员，修改指定对象的NSET名称
-        Node.a_line[Node.ls-1] = Node.a_line[Node.ls-1][0:Node.a_line[Node.ls-1].index('=')+1] + nset +'\n'
+    def set_nset(self, nset):    # 类成员，修改指定对象的NSET名称
+        self.inp_data[self.ls-1] = self.inp_data[self.ls-1][0:self.inp_data[self.ls-1].index('=')+1] + nset + '\n'  #将*NODE行第一个等号后的内容替换成节点组名字
         
-    def write(self, filepath):
-    #类成员，把最后处理好的节点信息写入制定的inp中
-        fw = open(filepath,'w')
-        fw.writelines(Node.a_line)
+    def write(self, output_path):  # 类成员，把最后处理好的节点信息写入制定的inp中
+        fw = open(output_path, 'w')
+        fw.writelines(self.inp_data)
         fw.close()
